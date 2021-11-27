a464ea4901a86cea8523de7d000d5d2ed634660c
Andrei Gorbulin <andrey1gorbulin@gmail.com>
2021-11-27 21:49:14 +0200

    Moved to conanfile.py and fixed conan installs on MasOS/Linux

---
+++ conanfile.py
@@ -0,0 +1,47 @@
+from conans import ConanFile
+
+
+class VulkanTutorialConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    requires = (
+        "sdl2/2.0.16@bincrafters/stable",
+        "glm/0.9.9.8",
+        "imgui/1.85",
+        "stb/cci.20210713",
+        "tinyobjloader/1.0.6",
+        "vk-bootstrap/0.4",
+        "vulkan-memory-allocator/2.3.0",
+        "vulkan-headers/1.2.195",
+        "volk/1.2.195",
+    )
+    generators = "cmake"
+    default_options = {
+        "sdl2:opengl": False,
+        "sdl2:opengles": False,
+        "sdl2:directx": False,
+    }
+
+    def imports(self):
+        self.copy(
+            "imgui_impl_sdl.cpp",
+            dst="../../src/bindings",
+            src="./res/bindings",
+        )
+        self.copy(
+            "imgui_impl_sdl.h", dst="../../src/bindings", src="./res/bindings"
+        )
+        self.copy(
+            "imgui_impl_vulkan.cpp",
+            dst="../../src/bindings",
+            src="./res/bindings",
+        )
+        self.copy(
+            "imgui_impl_vulkan.h",
+            dst="../../src/bindings",
+            src="./res/bindings",
+        )
+
+    def configure(self):
+        # Disable iconv compilation on Windows, since it is not possible
+        if self.settings.os == "Windows":
+            self.options["sdl2"].iconv = False
